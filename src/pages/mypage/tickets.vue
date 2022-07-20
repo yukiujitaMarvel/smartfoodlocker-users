584fa259f4f910d0f9e760ad9358690edaab5b2a
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-07-20 14:40:07 +0900

    チケットレイアウト実装

--- src/pages/mypage/tickets.vue
+++ src/pages/mypage/tickets.vue
@@ -3,60 +3,82 @@
     <Header />
     <HeaderDetail :title="myTitle" />
     <div class="mypage-info-wrap">
-      <div class="mypage-info-inner-wrap">
-        <div class="mypege-header-wrap">
-          <div class="mypage-header">
-            <h1>回数チケット一覧</h1>
-          </div>
-          <div class="mypage-contents">
-            <div class="ticket-wrap"> 
-              <div class="ticket-img">
-                <img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" alt="">
-              </div>
-              <div class="ticket-contents">
-                <h3>test</h3>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-              </div>
-              <Modal />
+      <div class="ticket-wrap">
+        <div class="cart">
+        </div>
+        <div class="ticket-content">
+          <div class="ticket-left">
+            <div class="ticket-left-content">
+              <h5>smartfoodlocker</h5>
+              <p>お弁当チケット</p>
             </div>
           </div>
-          <div class="mypage-contents">
-            <div class="ticket-wrap"> 
-              <div class="ticket-img">
-                <img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" alt="">
-              </div>
-              <div class="ticket-contents">
-                <h3>test</h3>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-              </div>
-              <Modal />
+          <div class="ticket-right">
+            <div class="ticket-right-content">
+              <p>残り<span>10枚</span></p>
             </div>
           </div>
-          <div class="mypage-contents">
-            <div class="ticket-wrap"> 
-              <div class="ticket-img">
-                <img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" alt="">
-              </div>
-              <div class="ticket-contents">
-                <h3>test</h3>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-                <p>testtesttest</p>
-              </div>
-              <Modal />
-            </div>
-          </div>
-          
-          
         </div>
       </div>
+
+      <div class="ticket-wrap">
+        <div class="cart">
+            <a href="#"><font-awesome-icon icon="fa-solid fa-cart-arrow-down" style="color:#EA5303; font-size:20px;" /></a>
+        </div>
+        <div class="ticket-content">
+          <div class="ticket-left">
+            <div class="ticket-left-content">
+              <h5>smartfoodlocker</h5>
+              <p>お弁当チケット</p>
+            </div>
+          </div>
+          <div class="ticket-right">
+            <div class="ticket-right-content">
+              <p>10枚+1枚おまけ</p>
+              <p>¥5,500 → <span>¥5,000</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ticket-wrap">
+        <div class="cart">
+            <a href="#"><font-awesome-icon icon="fa-solid fa-cart-arrow-down" style="color:#EA5303; font-size:20px;" /></a>
+        </div>
+        <div class="ticket-content">
+          <div class="ticket-left">
+            <div class="ticket-left-content">
+              <h5>smartfoodlocker</h5>
+              <p>お弁当チケット</p>
+            </div>
+          </div>
+          <div class="ticket-right">
+            <div class="ticket-right-content">
+              <p>20枚+3枚おまけ</p>
+              <p>¥11,500 → <span>¥10,000</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ticket-wrap">
+        <div class="cart">
+            <a href="#"><font-awesome-icon icon="fa-solid fa-cart-arrow-down" style="color:#EA5303; font-size:20px;" /></a>
+        </div>
+        <div class="ticket-content">
+          <div class="ticket-left">
+            <div class="ticket-left-content">
+              <h5>smartfoodlocker</h5>
+              <p>お弁当チケット</p>
+            </div>
+          </div>
+          <div class="ticket-right">
+            <div class="ticket-right-content">
+              <p>30枚+5枚おまけ</p>
+              <p>¥17,500 → <span>¥15,000</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
     <Footer />
   </div>
@@ -65,7 +87,6 @@
 <script>
 import Header from '~/components/Header'
 import HeaderDetail from '~/components/HeaderDetail'
-import Modal from '~/components/Modal'
 import Footer from '~/components/Footer'
 
 export default {
@@ -76,13 +97,12 @@ export default {
   },
   data () {
   return {
-     myTitle: '回数チケット'
+     myTitle: 'チケット'
     }
   },
   components: {
     Header,
     HeaderDetail,
-    Modal,
     Footer
   },
   
@@ -95,90 +115,45 @@ export default {
   height: 100%;
   padding: 20px;
   background-color: white;
-
 }
-.mypage-info-inner-wrap{
-  width: 60%;
-  height: 100%;
-  margin: 0 auto;
-  box-shadow: 0 0 8px gray;
-}
-.mypage-header{
-  background-color: #EA5303;
-  color: white;
-  padding: 5px 10px;
-}
-.mypage-header h1{
+p{
+  margin: 0;
   font-size: 14px;
 }
-.mypage-contents{
-  width: 60%;
-  margin: 0 auto;
-  padding: 20px;
-  text-align: center;
-  color: black;
-}
 .ticket-wrap{
-  width: 100%;
-  border-radius: 5px;
-  border: 2px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+
 }
-.ticket-img{
-  width: 100%;
-  height: 150px;
+.ticket-content{
+  display: flex;
+  width: 89%;
+  color: white;
+  background-color: #EA5303;
+  align-items: center;
+  justify-content: space-between;
 }
-.ticket-img img {
-  width: 100%;
-  height: 100%;
+.ticket-left{
+  text-align: center;
+  border-right: 1px solid white;
+  width: calc(100%/2);
 }
-.ticket-contents{
-  text-align: left;
+.ticket-left-content{
   padding: 10px;
 }
-.ticket-contents h3 {
-  margin-bottom: 16px;
+.ticket-right{
+  text-align: center;
 }
-.ticket-contents p{
-  font-size: 12px;
+.ticket-right-content{
+  padding: 10px;
 }
-.ticket-btn{
-  background-color: #EA5303;
-  color: white;
-  font-weight: bold;
-  cursor: pointer;
-}
-.ticket-btn p {
-  margin: 0;
-  padding-top: 10px;
-}
-.ticket-btn:hover {
-  background-color: #e96c29;
-}
-span{
-  font-size: 10px;
-}
-h2{
+.ticket-right-content span{
   font-size: 16px;
+  font-weight: bold;
 }
 
-@media screen and (max-width: 480px) {
-  .mypage-info-wrap{
-    padding: 50px 0;
-  }
-  .mypage-info-inner-wrap{
-    width: 100%;
-    height: 100%;
-    margin: 0 auto;
-    box-shadow: 0 0 8px gray;
-  }
-  .mypage-contents{
-  width: 100%;
-  margin: 0 auto;
-  padding: 20px;
-  text-align: center;
-  color: black;
-}
-}
 </style>
 
 
